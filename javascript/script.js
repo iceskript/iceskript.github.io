84ab219161abf1512b14860c72bf51d467d88916
iceskript <randinova28@gmail.com>
2025-03-12 07:51:15 +0800

    Update DOM

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,6 @@
-const app = document.getElementById('app')
+
+const app = document.getElementById('app');
+const header = document.createElement('h1');
+const headerContent = document.createTextNode("Hi! I'm IceSkript");
+header.appendChild(headerContent);
+app.appendChild(header);
