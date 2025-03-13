f45976991efe252551f8cf9d1a878e820939f4e9
iceskript <randinova28@gmail.com>
2025-03-13 21:49:56 +0800

    added git ignore

--- javascript/script.js
+++
@@ -1,6 +0,0 @@
-
-const app = document.getElementById('app');
-const header = document.createElement('h1');
-const headerContent = document.createTextNode("Hi! I'm IceSkript");
-header.appendChild(headerContent);
-app.appendChild(header);
